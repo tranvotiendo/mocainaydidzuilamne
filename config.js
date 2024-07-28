5fb80b1cbb5ec3e041db445b550500e2af3aa1b0
tranvotiendo <110394763+tranvotiendo@users.noreply.github.com>
2024-07-28 22:37:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'mtamm à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `t có bất ngờ cho m nè kkk`,
     btnIntro: 'iu em:))',
     title: 'Trả lời thật đi 😙',
     desc: 'mtamm có đáng ghéc không ',
-    btnYes: 'mtamm dthw vc <33',
-    btnNo: 'Không, mtamm thấy ghéc vc',
+    btnYes: 'Không, mtamm thấy ghéc vc',
+    btnNo: 'mtamm dthw vc <33',
     question:'droi, nhma tsao mtamm lại thấy ghéc nhờ <3',
     btnReply: 'gửi cho tdoo <3',
     reply: 'tai vi anh co 2 con cu <33333333',
     mess: 'em biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'chúc mừng anh đã bị hack face:>',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/tranvotiendo' //link mess
+    messLink: 'https://www.facebook.com/profile.php?id=100024986162766' //link mess
 }
